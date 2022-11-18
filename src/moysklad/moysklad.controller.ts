c51a8ed7ee3a444d0ffe50c862bd2b9b2a1a4cd9
Andrey Shkarin <shkarinandrew@mail.ru>
2022-11-19 00:42:51 +0300

    feat: Добавил поиск по параметру stateName

--- src/moysklad/moysklad.controller.ts
+++ src/moysklad/moysklad.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
+import { Query } from '@nestjs/common/decorators';
+import { UpdateProfitDto } from './dto/update-profit.dto';
 import { MoyskladService } from './moysklad.service';
 
 @Controller('api')
@@ -6,8 +8,10 @@ export class MoyskladController {
   constructor(private readonly moyskladService: MoyskladService) {}
 
   @Get('moysklad')
-  async getProfits() {
-    const profits = await this.moyskladService.getAllProfit();
+  async getProfits(@Query() stateName: UpdateProfitDto['stateName']) {
+    console.log(stateName);
+
+    const profits = await this.moyskladService.getAllProfit(stateName);
 
     return profits;
   }
